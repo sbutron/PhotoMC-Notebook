0a1fb666bb73f95b76075040235655f90b55b63b
sbutron <73725598+sbutron@users.noreply.github.com>
2021-04-27 09:47:15 -0400

    Add files via upload

---
+++ notebook.jl
@@ -0,0 +1,22 @@
+### A Pluto.jl notebook ###
+# v0.14.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 224a4830-a75e-11eb-2f74-3783afe29fe9
+begin
+	import Pkg
+	Pkg.activate(mktempdir())
+	Pkg.add("PlutoUI")
+	Pkg.add(url = "https://github.com/sbutron/PhotoMC.jl")
+	using PlutoUI
+	using PhotoMC
+end
+
+# ╔═╡ 2346f4f7-2943-406d-80be-36813bacb15a
+
+
+# ╔═╡ Cell order:
+# ╠═224a4830-a75e-11eb-2f74-3783afe29fe9
+# ╠═2346f4f7-2943-406d-80be-36813bacb15a
